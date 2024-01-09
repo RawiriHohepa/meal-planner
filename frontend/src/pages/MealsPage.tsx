d89593ab2f85bffb20cfe4a12eeb2cea417f6b4d
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2024-01-09 15:37:43 +1300

    Add tab panels for meals

--- frontend/src/pages/MealsPage.tsx
+++ frontend/src/pages/MealsPage.tsx
@@ -1,29 +1,83 @@
-import { Typography, Paper } from "@mui/material";
+import { useState } from "react";
+import { Box, Toolbar, Divider, Paper, Tabs, Tab } from "@mui/material";
+
+const meals = [
+  {
+    _id: "111",
+    name: "Meal One",
+  },
+  {
+    _id: "222",
+    name: "Meal Two",
+  },
+  {
+    _id: "333",
+    name: "Meal Three",
+  },
+  {
+    _id: "444",
+    name: "Meal Four",
+  },
+];
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel = ({ children, value, index, ...other }: TabPanelProps) => (
+  <div
+    role="tabpanel"
+    hidden={value !== index}
+    id={`simple-tabpanel-${index}`}
+    aria-labelledby={`simple-tab-${index}`}
+    {...other}
+  >
+    {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+  </div>
+);
+
+const a11yProps = (index: number) => ({
+  id: `simple-tab-${index}`,
+  "aria-controls": `simple-tabpanel-${index}`,
+});
 
 const MealsPage = () => {
+  const [selectedMealIndex, setSelectedMealIndex] = useState(0);
+
   return (
-    <Paper elevation={2} sx={{ p: 3 }}>
-      <Typography paragraph>Meals</Typography>
-      <Typography paragraph>
-        Exorcizamus te, omnis immundus spiritus, omnis satanica potestas, omnis
-        incursio infernalis adversarii, omnis legio, omnis congregatio et secta
-        diabolica. Ergo, draco maledicte. Ecclesiam tuam securi tibi facias
-        libertate servire, te rogamus, audi nos.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-        ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
-        integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-        lacus sed viverra tellus. Purus sit amet volutpat consequat mauris.
-        Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-        accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac.
-        Pellentesque nec nam aliquam sem et tortor. Habitant morbi tristique
-        senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
-        maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-        aliquam ultrices sagittis orci a.
-      </Typography>
+    <Paper elevation={2}>
+      <Box sx={{ display: "flex" }}>
+        <Box>
+          {/* TODO implement button and popup dialog */}
+          <Toolbar>New Meal</Toolbar>
+          <Divider />
+          <Tabs
+            value={selectedMealIndex}
+            orientation="vertical"
+            onChange={(_, newSelectedMealIndex) =>
+              setSelectedMealIndex(newSelectedMealIndex)
+            }
+          >
+            {meals.map((meal, index) => (
+              <Tab key={meal._id} label={meal.name} {...a11yProps(index)} />
+            ))}
+          </Tabs>
+        </Box>
+        <Divider orientation="vertical" flexItem />
+        <Box sx={{ width: "100%" }}>
+          {/* TODO implement toolbar */}
+          <Toolbar>New Ingredient Autocompleter & Delete button</Toolbar>
+          <Divider />
+          {/* TODO implement table */}
+          {meals.map((meal, index) => (
+            <TabPanel value={selectedMealIndex} index={index} key={meal._id}>
+              {meal.name}
+            </TabPanel>
+          ))}
+        </Box>
+      </Box>
     </Paper>
   );
 };
